4fe53c7ac8bada595c8575a8d33895c4b5c71e1d
eedelosreyes2 <delosreyes.elijah@gmail.com>
2022-02-07 19:13:48 -0800

    work and styling

--- pages/components/header.js
+++ pages/components/header.js
@@ -3,8 +3,8 @@ import { AiOutlineMenu } from 'react-icons/ai';
 
 export default function Header() {
   return (
-    <div className="flex justify-between py-5">
-      <div>Elijah Delos Reyes</div>
+    <div className="bg-slate-900 flex fixed justify-between px-10 py-5 w-full">
+      <div className="font-bold">Elijah Delos Reyes</div>
       <IconContext.Provider value={{ className: 'mt-1 cursor-pointer' }}>
         <AiOutlineMenu />
       </IconContext.Provider>
--- pages/components/work.js
+++ pages/components/work.js
@@ -1,20 +1,29 @@
 import { jobs } from '../clientData/work.json';
 
 export default function Work() {
-  console.log(jobs);
-
   return (
     <div>
       {jobs.map((job) => (
         <div className="py-5">
-          <div className="flex items-end mb-2">
-            <div className="font-bold text-2xl mr-2">{job.title}</div>
-            <div>at {job.company}</div>
+          <div className="flex-column items-end pb-2">
+            <div>
+              <a
+                href={job.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="font-bold text-cyan-500"
+              >
+                {job.company}
+              </a>
+            </div>
+            <div className="font-bold text-2xl">{job.title}</div>
           </div>
           <div className="mb-2">{job.description}</div>
-          <div className="flex">
+          <div className="flex flex-wrap">
             {job.skills.map((skill) => (
-              <div className="font-bold mr-2">{skill}</div>
+              <div className="font-bold text-violet-600 text-sm pr-3">
+                {skill}
+              </div>
             ))}
           </div>
         </div>
